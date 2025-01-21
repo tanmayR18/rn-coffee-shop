309debb7f39dda7d70682896ab5e507fb46ba565
tanmayrane18 <tanmayrane51@gmail.com>
2025-01-21 23:13:03 +0530

    feat(store): added zustand store

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import { useStore } from '../store/store';
+
+const getCatgeoriesFromData = (data: any) => {
+  let temp: any = {};
+  for (let i = 0; i < data.length; i++) {
+    if (temp[data[i].name] === undefined) {
+      temp[data[i].name] = 1;
+    } else {
+      temp[data[i].name]++;
+    }
+    let categories = Object.keys(temp);
+    categories.unshift('All');
+    return categories;
+  }
+};
 
 const HomeScreen = () => {
+    const CoffeeList = useStore((state: any) => state.CoffeeList);
+    const BeanList = useStore((state: any) => state.BeanList);
+    const [ categories, setCategories] = useState(getCatgeoriesFromData(CoffeeList));
+    const [setsearchText, setSetsearchText] = useState(undefined)
+    const [categoryIndex, setCategoryIndex] = useState({
+        index: 0,
+        category: categories[0]
+    });
+    const [setsortedCoffee, setSetsortedCoffee] = useState(undefined)
   return (
     <View>
       <Text>HomeScreen</Text>
@@ -11,4 +35,4 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
