9bcfbeeaffb574b827dfc5c4347f4671e67a3b90
tanmayrane18 <tanmayrane51@gmail.com>
2025-02-23 22:22:06 +0530

    order histroy page layout created

--- src/screens/OrderHistoryScreen.tsx
+++ src/screens/OrderHistoryScreen.tsx
@@ -1,9 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
+import {COLORS} from '../theme/theme';
+import {ScrollView} from 'react-native-gesture-handler';
+import {getTabBarHeight} from '@react-navigation/bottom-tabs/lib/typescript/src/views/BottomTabBar';
 
 const OrderHistoryScreen = () => {
+  const tabBarHeight = getTabBarHeight();
+  const OrderHistoreList = useStore((state: any) => state.OrderHistoreList);
+  console.log('History=', OrderHistoreList.length);
   return (
-    <View>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFLex}>
+        <View
+          style={[styles.ScrollViewInnerView, {marginBottom: tabBarHeight}]}>
+          <View style={styles.ItemContainer}></View>
+        </View>
+      </ScrollView>
       <Text>OrderHistoryScreen</Text>
     </View>
   );
@@ -11,4 +28,19 @@ const OrderHistoryScreen = () => {
 
 export default OrderHistoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFLex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+});
